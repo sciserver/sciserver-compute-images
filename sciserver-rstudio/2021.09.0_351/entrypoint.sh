9199d06f5c9219ea2861e5f9523b80d80e984961
Dmitry Medvedev <dmedved1@jhu.edu>
2021-10-04 02:35:16 -0400

    Update all images

--- sciserver-rstudio/2021.09.0_351/entrypoint.sh
+++ sciserver-rstudio/2021.09.0_351/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sed -i "s+SCISERVER_PATH+$1+g" /etc/nginx/nginx.conf
+sed -i "s|SCISERVER_PATH|$1|g" /etc/nginx/nginx.conf
+sed -i 's|Ncpus=[0-9]\+|Ncpus=4|g' /home/idies/.Rprofile
 
 nginx
 
 cd /home/idies/workspace
 
 exec sudo -u idies env PATH=$PATH /usr/lib/rstudio-server/bin/rserver
-

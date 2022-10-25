a59cd69db2f9112364fd55e305dadc7d2aeaf406
Abdu Zoghbi <a.zoghbi@nasa.gov>
2022-10-25 14:48:35 -0400

    updated the main heasarc image

--- heasarc/heasarc/startup.sh
+++ heasarc/heasarc/startup.sh
@@ -3,16 +3,16 @@
 export SHELL=/bin/bash
 
 # find conda environments in user volumes and add to kernel list
-# echo "$(date) - scanning for conda environments"
-# for uv in /home/idies/workspace/{Storage,Temporary}/*/*; do
-#     if [[ -d ${uv}/conda-meta ]]; then
-#         name=$(basename $uv)
-#         displayname="$name (uservolume environment)"
-#         echo "$(date) - adding environment $displayname"
-#         ${uv}/bin/python -m ipykernel install --user --name "$name" --display-name "$displayname"
-#     fi
-# done
-# echo "$(date) - completed scanning for conda environments. Starting Jupyter server"
+echo "$(date) - scanning for conda environments"
+for uv in /home/idies/workspace/{Storage,Temporary}/*/*; do
+    if [[ -d ${uv}/conda-meta ]]; then
+        name=$(basename $uv)
+        displayname="$name (uservolume environment)"
+        echo "$(date) - adding environment $displayname"
+        ${uv}/bin/python -m ipykernel install --user --name "$name" --display-name "$displayname"
+    fi
+done
+echo "$(date) - completed scanning for conda environments. Starting Jupyter server"
 
 
 jupyter lab \

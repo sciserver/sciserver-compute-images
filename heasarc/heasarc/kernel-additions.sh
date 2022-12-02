eb125dbfd238df31e66063f1d7e18767506a9ee0
Abdu Zoghbi <a.zoghbi@nasa.gov>
2022-12-02 10:08:27 -0500

    added the optional SciScript-Python package, and moved kernel-related installation to a separate script

---
+++ heasarc/heasarc/kernel-additions.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+
+## A few additions to the kernels ##
+## ------------------------------ ##
+
+# needed in the loop
+cd ~
+git clone -b sciserver-v2.1.0 --depth=1 https://github.com/sciserver/SciScript-Python.git
+
+for env in  heasoft ciao fermi xmmsas; do
+    
+    echo "-- Working on conda environment $env --"
+
+    # Add conda envs to jupyter
+    mamba install -y -n $env ipykernel
+    mamba run -n $env python -m ipykernel install --user --name=$env --display-name="($env)"
+
+    # A fix that ensures notebooks load the scripts in /env/etc/conda/activate.d
+    sed "s/heasoft/$env/" kernel.json > ~/.local/share/jupyter/kernels/$env/kernel.json
+    
+    # Install sciserver python package
+    # https://github.com/sciserver/sciserver-compute-images/blob/master/essentials/3.0/sciserver-essentials/Dockerfile#L154
+    touch ~/keystone.token
+    cd ~/SciScript-Python/py3
+    mamba run -n $env python setup.py install
+    cd ../..
+    rm ~/keystone.token
+
+done
+
+rm -rf ~/SciScript-Python

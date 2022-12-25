4182307c51d48f1a2943a59d2f17358a2bc0aee2
Abdu Zoghbi <a.zoghbi@nasa.gov>
2022-12-25 14:44:29 -0500

    added js9 and vnc to the heasarc docker file, and added jupyter to all kernels

---
+++ heasarc/heasarc/ds9/config/jupyter_notebook_config.py
@@ -0,0 +1,33 @@
+import shlex
+
+
+import os
+import shlex
+from shutil import which
+
+
+
+def setup_ds9():
+
+    out = {
+        'command': ['start_supervisor'],
+        'port': 5901,
+        'timeout': 30,
+        'mappath': {'/': '/vnc_lite.html'},
+        'new_browser_window': False,
+        'launcher_entry': {
+            'enabled': True,
+            'icon_path': '/opt/ds9.svg',
+            'title': 'DS9'
+        }
+    }
+    return out
+
+
+c.ServerProxy.servers = {
+    'ds9': setup_ds9()
+}
+
+
+
+
